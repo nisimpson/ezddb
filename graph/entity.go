f9042c4a9b0b5c6f3f3401ce6124dd5ef1e451c0
Nathan Simpson <nathanisaac@gmail.com>
2024-11-14 19:56:51 -0800

    feat: add docs

--- graph/entity.go
+++ graph/entity.go
@@ -10,15 +10,34 @@ import (
 	"github.com/nisimpson/ezddb/operation"
 )
 
-type Node interface {
-	DynamoNodeID() string
-	DynamoNodePrefix() string
-	DynamoNodeType() string
-	DynamoNodeRelationships() map[string]Node
-	DynamoNodeRefIsReverseLookup(relation string) bool
-	DynamoNodeRef(relation string) []Node
-	SetDynamoNodeRef(relation string, refID string)
-	SetDynamoNodeTimestamp(created, updated time.Time)
+// Entity represents a singular "thing" within an entity-relationship model.
+// Entities can form relationships with other entities; these associations
+// are stored within the dynamodb table as separate rows within the table.
+// Use the [Graph] client to perform CRUD operations on stored entities.
+type Entity interface {
+	// DynamoID returns the unique identifier associated with the entity.
+	DynamoID() string
+	// DynamoEntityType returns the type of entity. This is used to
+	// partition the collection of similar entities in the table's
+	// global secondary index.
+	DynamoEntityType() string
+	// DynamoRelationships returns a mapping of this entity's relationships
+	// to other [Entity] types. The key is the relationship name, and the value is
+	// the relationship target's entity type.
+	DynamoRelationships() map[string]string
+	// DynamoIsReverseRelationship returns true if the relationship is stored
+	// in reverse order. This is used to optimize reverse lookups.
+	DynamoIsReverseRelationship(relation string) bool
+	// DynamoGetRelationship returns a list of entities that are related to
+	// this [Entity] by the given relationship.
+	DynamoGetRelationship(relation string) []Entity
+	// DynamoSetRelationship is called when a relationship between this [Entity] and the
+	// target [Entity] is defined.
+	DynamoSetRelationship(relation string, entityID string)
+	// DynamoSetTimestamp provides the creation date and the last modification
+	// date for this entity. [Entity] implementations should use the provided
+	// values to update timestamp information if applicable.
+	DynamoSetTimestamp(created, updated time.Time)
 }
 
 type nodeRef struct {
@@ -31,7 +50,7 @@ type nodeRef struct {
 	TargetNodeType   string `dynamodbav:"targetNodeType"`
 }
 
-func newNodeRef(source, target Node, relation string, reverse bool) nodeRef {
+func newNodeRef(source, target Entity, relation string, reverse bool) nodeRef {
 	src, tgt := source, target
 	if reverse {
 		// relationships are stored in reverse order
@@ -42,12 +61,12 @@ func newNodeRef(source, target Node, relation string, reverse bool) nodeRef {
 	}
 	return nodeRef{
 		Relationship:     relation,
-		SourceNodeID:     src.DynamoNodeID(),
-		SourceNodePrefix: src.DynamoNodePrefix(),
-		SourceNodeType:   src.DynamoNodeType(),
-		TargetNodeID:     tgt.DynamoNodeID(),
-		TargetNodePrefix: tgt.DynamoNodePrefix(),
-		TargetNodeType:   tgt.DynamoNodeType(),
+		SourceNodeID:     src.DynamoID(),
+		SourceNodePrefix: src.DynamoEntityType(),
+		SourceNodeType:   src.DynamoEntityType(),
+		TargetNodeID:     tgt.DynamoID(),
+		TargetNodePrefix: tgt.DynamoEntityType(),
+		TargetNodeType:   tgt.DynamoEntityType(),
 	}
 }
 
@@ -62,27 +81,54 @@ func (n nodeRef) DynamoMarshalRecord(options *MarshalOptions) {
 	options.SupportCollectionQuery = false
 }
 
-type edge[T Node] struct {
+type edge[T Entity] struct {
 	Node T `dynamodbav:"node"`
 }
 
-func (e edge[T]) DynamoItemType() string { return e.Node.DynamoNodeType() }
+func (e edge[T]) DynamoItemType() string { return e.Node.DynamoEntityType() }
 
 func (e edge[T]) DynamoMarshalRecord(options *MarshalOptions) {
-	options.HashKeyID = e.Node.DynamoNodeID()
-	options.SortKeyID = e.Node.DynamoNodeID()
-	options.HashKeyPrefix = e.Node.DynamoNodePrefix()
-	options.SortKeyPrefix = e.Node.DynamoNodePrefix()
+	options.HashKeyID = e.Node.DynamoID()
+	options.SortKeyID = e.Node.DynamoID()
+	options.HashKeyPrefix = e.Node.DynamoEntityType()
+	options.SortKeyPrefix = e.Node.DynamoEntityType()
 	options.SupportReverseLookup = true
 	options.SupportCollectionQuery = true
 }
 
-type Graph[T Node] struct {
+// Graph is an abstraction layer over the base dynamodb client,
+// treating the target table as a graph-based data structure.
+// The table itself is used as an 2D adjacency matrix of
+// nodes:
+//
+//	    A  B  C  D
+//	  ------------
+//	A | 1  1  0  0
+//	B | 1  1  0  0
+//	C | 0  0  1  0
+//	D | 0  0  0  1
+//
+// Each cell in the matrix represents a directed edge from source
+// nodes rows and target row columns. In the dynamodb table,
+// table items represent edges to nodes within the graph. An edge
+// is uniquely identified by its composite key; the hash key refers
+// to the source node id while the sort key refers to the target node id.
+//
+// Nodes are represented by table partition; in this way, a node
+// can be seen as the composition of its edges. Information about the node
+// is stored in the "identity edge", or dynamodb item with an equivalent
+// hash and sort key.
+//
+// Edge direction is represented by the partition where the item
+// is stored; Given nodes A and B, edge A -> B is stored on partition A
+// and is queried on node A. In order to query the reverse edge B -> A,
+// Graph uses the reverse lookup index on node B.
+type Graph[T Entity] struct {
 	nodes Table[edge[T]]
 	refs  Table[nodeRef]
 }
 
-func New[T Node](tableName string, opts ...func(*Options)) Graph[T] {
+func New[T Entity](tableName string, opts ...func(*Options)) Graph[T] {
 	return Graph[T]{
 		nodes: NewTable[edge[T]](tableName, opts...),
 		refs:  NewTable[nodeRef](tableName, opts...),
@@ -90,13 +136,13 @@ func New[T Node](tableName string, opts ...func(*Options)) Graph[T] {
 }
 
 func (g Graph[T]) refsOf(node T) []nodeRef {
-	refs := node.DynamoNodeRelationships()
-	items := make([]nodeRef, 0, len(refs))
+	relationships := node.DynamoRelationships()
+	items := make([]nodeRef, 0, len(relationships))
 
-	for relation := range refs {
-		reverse := node.DynamoNodeRefIsReverseLookup(relation)
-		for _, ref := range node.DynamoNodeRef(relation) {
-			record := newNodeRef(node, ref, relation, reverse)
+	for relationship := range relationships {
+		reverse := node.DynamoIsReverseRelationship(relationship)
+		for _, ref := range node.DynamoGetRelationship(relationship) {
+			record := newNodeRef(node, ref, relationship, reverse)
 			items = append(items, record)
 		}
 	}
@@ -141,8 +187,8 @@ func (g Graph[T]) DeleteNode(node T, opts ...func(*Options)) operation.Delete {
 func (g Graph[T]) DeleteNodeEdges(node T, relation string, opts ...func(*Options)) operation.BatchWriteItemCollection {
 	g.refs.options.apply(opts)
 	var (
-		reverse    = node.DynamoNodeRefIsReverseLookup(relation)
-		refs       = node.DynamoNodeRef(relation)
+		reverse    = node.DynamoIsReverseRelationship(relation)
+		refs       = node.DynamoGetRelationship(relation)
 		collection = make(operation.BatchWriteItemCollection, 0, len(refs))
 	)
 
@@ -155,12 +201,12 @@ func (g Graph[T]) DeleteNodeEdges(node T, relation string, opts ...func(*Options
 
 func (g Graph[T]) DeleteAllNodeEdges(node T, opts ...func(*Options)) operation.BatchWriteItemCollection {
 	var (
-		definitions = node.DynamoNodeRelationships()
-		collection  = make(operation.BatchWriteItemCollection, 0)
+		relationships = node.DynamoRelationships()
+		collection    = make(operation.BatchWriteItemCollection, 0)
 	)
 
-	for def := range definitions {
-		collection = append(collection, g.DeleteNodeEdges(node, def)...)
+	for relationship := range relationships {
+		collection = append(collection, g.DeleteNodeEdges(node, relationship)...)
 	}
 
 	return collection
@@ -174,7 +220,7 @@ func (g Graph[T]) UnmarshalNode(item ezddb.Item, opts ...func(*Options)) (T, err
 		return node, err
 	}
 	node = record.Data.Node
-	node.SetDynamoNodeTimestamp(record.CreatedAt, record.UpdatedAt)
+	node.DynamoSetTimestamp(record.CreatedAt, record.UpdatedAt)
 	return node, nil
 }
 
@@ -212,19 +258,19 @@ func (g Graph[T]) UnmarshalEdge(node T, item ezddb.Item, opts ...func(*Options))
 
 	var (
 		relationship = record.Data.Relationship
-		reverse      = node.DynamoNodeRefIsReverseLookup(relationship)
+		reverse      = node.DynamoIsReverseRelationship(relationship)
 	)
 
 	if reverse {
-		node.SetDynamoNodeRef(relationship, record.Data.SourceNodeID)
+		node.DynamoSetRelationship(relationship, record.Data.SourceNodeID)
 	} else {
-		node.SetDynamoNodeRef(relationship, record.Data.TargetNodeID)
+		node.DynamoSetRelationship(relationship, record.Data.TargetNodeID)
 	}
 
 	return nil
 }
 
-type ListNodesQueryBuilder[T Node] struct {
+type ListNodesQueryBuilder[T Entity] struct {
 	node   T
 	graph  Graph[T]
 	cursor string
@@ -232,9 +278,9 @@ type ListNodesQueryBuilder[T Node] struct {
 	limit  int
 }
 
-func (b ListNodesQueryBuilder[T]) BuildQuery(opts ...func(*Options)) operation.Query {
+func (b ListNodesQueryBuilder[T]) Query(opts ...func(*Options)) operation.Query {
 	return b.graph.nodes.Query(CollectionQuery{
-		ItemType: b.node.DynamoNodeType(),
+		ItemType: b.node.DynamoEntityType(),
 		Cursor:   b.cursor,
 		Filter:   b.filter,
 		Limit:    b.limit,
@@ -256,11 +302,11 @@ func (b *ListNodesQueryBuilder[T]) WithFilter(filter expression.ConditionBuilder
 	return b
 }
 
-func (g Graph[T]) ListNodesQueryBuilder(node T, relation string) ListNodesQueryBuilder[T] {
+func (g Graph[T]) ListNodes(node T, relation string) ListNodesQueryBuilder[T] {
 	return ListNodesQueryBuilder[T]{node: node, graph: g}
 }
 
-type ListEdgesQueryBuilder[T Node] struct {
+type ListEdgesQueryBuilder[T Entity] struct {
 	node     T
 	relation string
 	graph    Graph[T]
@@ -269,27 +315,27 @@ type ListEdgesQueryBuilder[T Node] struct {
 	limit    int
 }
 
-func (g Graph[T]) ListEdgesQueryBuilder(node T, relation string) ListEdgesQueryBuilder[T] {
+func (g Graph[T]) ListNodeEdges(node T, relation string) ListEdgesQueryBuilder[T] {
 	return ListEdgesQueryBuilder[T]{node: node, relation: relation, graph: g}
 }
 
-func (b ListEdgesQueryBuilder[T]) BuildQuery(opts ...func(*Options)) operation.Query {
+func (b ListEdgesQueryBuilder[T]) Query(opts ...func(*Options)) operation.Query {
 	b.graph.nodes.options.apply(opts)
 	b.graph.refs.options.apply(opts)
 
 	var (
-		definitions    = b.node.DynamoNodeRelationships()
-		def            = definitions[b.relation]
-		marshalOptions = b.graph.nodes.options.MarshalOptions
-		record         = Marshal(edge[T]{Node: b.node}, marshalOptions...)
+		relationships     = b.node.DynamoRelationships()
+		relatedEntityType = relationships[b.relation]
+		marshalOptions    = b.graph.nodes.options.MarshalOptions
+		record            = Marshal(edge[T]{Node: b.node}, marshalOptions...)
 	)
 
 	// perform a reverse lookup for the edges forming the target
 	// relationship.
-	if b.node.DynamoNodeRefIsReverseLookup(b.relation) {
+	if b.node.DynamoIsReverseRelationship(b.relation) {
 		return b.graph.refs.Query(ReverseLookupQuery{
 			SortKeyValue:      record.SK,
-			GSI1SortKeyPrefix: def.DynamoNodePrefix(),
+			GSI1SortKeyPrefix: relatedEntityType,
 			Cursor:            b.cursor,
 			Limit:             b.limit,
 		})
@@ -299,7 +345,7 @@ func (b ListEdgesQueryBuilder[T]) BuildQuery(opts ...func(*Options)) operation.Q
 	// relationship.
 	return b.graph.refs.Query(LookupQuery{
 		PartitionKeyValue: record.HK,
-		SortKeyPrefix:     def.DynamoNodePrefix(),
+		SortKeyPrefix:     relatedEntityType,
 		Cursor:            b.cursor,
 		Limit:             b.limit,
 	})
